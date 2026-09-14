cb6cab85ad06ef8ea9603a53b709be22f0b0f965
Nico <carlson.nico@gmail.com>
2026-09-14 14:03:58 -0600

    added a few pages and simple navigation

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,17 @@ void main() {
     home: HomePage(),
     debugShowCheckedModeBanner: false,
     routes: <String, WidgetBuilder>{
-      "/Homepage": (BuildContext context) => HomePage(),
+      "/HomePage": (BuildContext context) => HomePage(),
       "/SecondPage": (BuildContext context) => SecondPage(),
+      "/ThirdPage": (BuildContext context) => ThirdPage(),
+      "/FourthPage": (BuildContext context) => FourthPage(),
     }
   ));
 }
 
 class HomePage extends StatelessWidget{
+  const HomePage({super.key});
+
 
   @override
   Widget build(BuildContext context){
@@ -52,11 +56,23 @@ class HomePage extends StatelessWidget{
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               IconButton(
-                onPressed: Navigator.of(context).pushNamed("/SecondPage"),
+                onPressed: (){Navigator.of(context).pushNamed("/SecondPage");},
                 icon: Icon(Icons.flight),
                 padding: EdgeInsets.zero,
                 iconSize: 70.0,
-              )
+              ),
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/ThirdPage");},
+                icon: Icon(Icons.bike_scooter),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/FourthPage");},
+                icon: Icon(Icons.fire_hydrant_alt_sharp),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
             ],
           )
 
@@ -67,4 +83,207 @@ class HomePage extends StatelessWidget{
   }
 
 
-}
+} // end of HomePage class
+
+
+
+class SecondPage extends StatelessWidget{
+  const SecondPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context){
+
+    // build the scaffold
+    return Scaffold(
+
+      // appbar
+      appBar: AppBar(
+        title: const Text("SecondPage"),
+        backgroundColor: Colors.orange,
+      ),
+
+      // body
+      body: ListView(
+        children: <Widget>[
+          // use a column to stack the widgets
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  padding: const EdgeInsetsDirectional.all(40.0),
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "This is\nmy travel app.\nPage Two",
+                    style: TextStyle(fontSize: 36.0),
+                  )),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/HomePage");},
+                icon: Icon(Icons.home),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/ThirdPage");},
+                icon: Icon(Icons.bike_scooter),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/FourthPage");},
+                icon: Icon(Icons.fire_hydrant_alt_sharp),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+            ],
+          )
+
+        ],
+      ),
+
+    );
+  }
+
+
+} // end of SecondPage class
+
+
+class ThirdPage extends StatelessWidget{
+  const ThirdPage({super.key});
+
+
+  @override
+  Widget build(BuildContext context){
+
+    // build the scaffold
+    return Scaffold(
+
+      // appbar
+      appBar: AppBar(
+        title: const Text("ThirdPage"),
+        backgroundColor: Colors.orange,
+      ),
+
+      // body
+      body: ListView(
+        children: <Widget>[
+          // use a column to stack the widgets
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  padding: const EdgeInsetsDirectional.all(40.0),
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "This is\nmy travel app.\nPage Three",
+                    style: TextStyle(fontSize: 36.0),
+                  )),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/HomePage");},
+                icon: Icon(Icons.home),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/SecondPage");},
+                icon: Icon(Icons.flight),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/FourthPage");},
+                icon: Icon(Icons.fire_hydrant_alt_sharp),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+            ],
+          )
+
+        ],
+      ),
+
+    );
+  }
+
+
+} // end of ThirdPage class
+
+class FourthPage extends StatelessWidget{
+  const FourthPage({super.key});
+
+
+
+
+  @override
+  Widget build(BuildContext context){
+
+    // build the scaffold
+    return Scaffold(
+
+      // appbar
+      appBar: AppBar(
+        title: const Text("FourthPage"),
+        backgroundColor: Colors.orange,
+      ),
+
+      // body
+      body: ListView(
+        children: <Widget>[
+          // use a column to stack the widgets
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  padding: const EdgeInsetsDirectional.all(40.0),
+                  color: Colors.blue,
+                  alignment: Alignment.center,
+                  child: const Text(
+                    "This is\nmy travel app.\nPage Four",
+                    style: TextStyle(fontSize: 36.0),
+                  )),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/HomePage");},
+                icon: Icon(Icons.home),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/SecondPage");},
+                icon: Icon(Icons.flight),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+              IconButton(
+                onPressed: (){Navigator.of(context).pushNamed("/ThirdPage");},
+                icon: Icon(Icons.bike_scooter),
+                padding: EdgeInsets.zero,
+                iconSize: 70.0,
+              ),
+            ],
+          )
+
+        ],
+      ),
+
+    );
+  }
+
+
+} // end of FourthPage class
